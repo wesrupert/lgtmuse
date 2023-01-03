10ccb67faff9f1222f4aecfe90ec8ecd0723058a
Wes Rupert <rupertwe@amazon.com>
2023-01-03 14:30:54 -0800

    Add LGTMuse landing page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,108 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="flex flex-col gap-8 container px-8 py-8 mx-auto">
+    <header class="flex flex-col">
+      <h1 class="text-4xl mb-2">LGTMuse</h1>
+      <h2 class="text-xl text-slate-500 mb">
+        A muse for what looks good to you
+      </h2>
+      <h3 class="text-xl italic text-slate-500">...on a book cover!</h3>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="relative rounded-2xl bg-slate-100 p-4">
+      <div class="flex flex-col gap-8">
+        <h2 class="capitalize text-xl">
+          <span>{{ title }}</span>
+          <span v-if="subtitle" class="capitalize text-base text-slate-500"
+            >...{{ subtitle }}</span
+          >
+        </h2>
+        <div class="text-base text-justify flex flex-col gap-2">
+          <div v-for="line in summary" :key="line">{{ line }}</div>
+        </div>
+      </div>
+      <LoaderEllipsis v-if="loading" />
+    </main>
+    <transition
+      enter-active-class="duration-300 ease-out"
+      enter-from-class="transform opacity-0 -translate-y-10"
+      leave-active-class="duration-300 ease-in"
+      leave-to-class="transform opacity-0 -translate-y-10"
+    >
+      <div
+        v-if="!loading"
+        class="self-center text-xl flex flex-col items-center"
+      >
+        <h2>Coming soon to a bookstore near you!</h2>
+        <h2>...Or, maybe not.</h2>
+        <button
+          class="mt-4 bg-blue-200 px-4 py-1 rounded-full"
+          @click="loadGoodTitleMetadata"
+        >
+          Try again?
+        </button>
+      </div>
+    </transition>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup lang="ts">
+import LoaderEllipsis from "./components/LoaderEllipsis.vue";
+import { onMounted, ref } from "vue";
+
+const TITLE_PLACEHOLDERS = [
+  "Looking...",
+  "Guessing...",
+  "Thinking...",
+  "Musing...",
+];
+
+const PLACEHOLDERS = [
+  "Figuring out a killer novel...",
+  "Determining the one book to rule them all...",
+  "Absorbing the universe, all for you and your book...",
+  "Vanquishing writer's block...",
+];
+
+function rval<T>(arr: T[]) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+const loading = ref(true);
+const title = ref("");
+const subtitle = ref("");
+const summary = ref<string[]>([]);
+
+async function loadGoodTitleMetadata() {
+  loading.value = true;
+  title.value = rval(TITLE_PLACEHOLDERS);
+  subtitle.value = "";
+  summary.value = [rval(PLACEHOLDERS)];
+
+  try {
+    const deriveReq = await fetch(
+      "https://blw6096nkc.execute-api.us-west-2.amazonaws.com/Prod/derive?acronym=LGTM"
+    );
+    if (!deriveReq.ok) {
+      throw new Error("Request failed.");
+    }
+    title.value = (await deriveReq.json()).derived;
+
+    const summaryReq = await fetch(
+      "https://blw6096nkc.execute-api.us-west-2.amazonaws.com/Prod/summarize?" +
+        new URLSearchParams({ title: title.value })
+    );
+    if (!summaryReq.ok) {
+      throw new Error("Request failed.");
+    }
+    const json = await summaryReq.json();
+    summary.value = json.summary;
+    subtitle.value = json.subtitle ?? subtitle.value;
+  } catch (err) {
+    console.error("Error occurred calling API:", err);
+  }
+
+  loading.value = false;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+onMounted(loadGoodTitleMetadata);
+</script>
